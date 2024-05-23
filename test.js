e6caf364ff030252e0ecf55e016f7352ab87f60d
Manase44 <fikopersempre@gail.com>
2024-05-23 13:31:32 +0300

    Handle conditional text entering

---
+++ test.js
@@ -0,0 +1,4 @@
+const unwantedKeys = ["Enter", "Shift", "CapsLock", " ", "ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight", "Backspace", "Control", "Alt", "Tab", "Menu", "WinKey"];
+
+
+  console.log(unwantedKeys.includes("Enter"));
